fd6dcfa45af21725eab9456b5159a6634554c8df
Andrew Shaffer <andrew@weo.io>
2016-05-03 14:22:27 -0700

    fix api url joining

--- src/lib/api/req.js
+++ src/lib/api/req.js
@@ -10,16 +10,18 @@ import map from '@f/map'
  * API Request Functions
  */
 
-const {get, put, post, del} = map(fn => xargs(fn, url => join(process.env.API_SERVER, url)), http)
+const {get, put, post, del} = map(fn =>
+  xargs(fn, url => join(process.env.API_SERVER, url)), http)
 
 /**
  * Join
  */
 
 function join (a, b) {
-  return a[a.length - 1] === '/' && b[0] === '/'
-    ? a + b.slice(1)
-    : a + b
+  if (a[a.length - 1] === '/') a = a.slice(-1)
+  if (b[0] === '/') b = b.slice(1)
+
+  return a + '/' + b
 }
 
 /**
