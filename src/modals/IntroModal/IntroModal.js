33b951f26515aa9c17ea8c1b6bdc63f998db9607
Michael Weintraub <mweint@gmail.com>
2016-05-18 12:40:27 -0700

    updated

--- src/modals/IntroModal/IntroModal.js
+++ src/modals/IntroModal/IntroModal.js
@@ -2,7 +2,7 @@
  * Imports
  */
 
-import {Block, Modal, Image, Icon, Flex} from 'vdux-ui'
+import {Block, Modal} from 'vdux-ui'
 import handleActions from '@f/handle-actions'
 import createAction from '@f/create-action'
 import IntroSteps from './IntroSteps'
@@ -34,32 +34,44 @@ function render ({props, state, local}) {
   }
 
   return (
-    <Modal relative w='670' h='535'>
+    <Modal relative w='670' h='535' overlayProps={{bg: 'rgba(51,51,51,0.96)'}}>
       {
         (cur < numSteps) && <IntroSteps cur={cur} counts={counts} />
       }
       {
         (cur >= numSteps) && <IntroForms cur={cur}/>
       }
-      <Block absolute top='100%' wide h={btnProps.circle} top  bottom m='auto' hide={cur >= numSteps}>
-        <Button
-          icon='keyboard_arrow_left'
-          onClick={local(prev)}
-          {...btnProps}
-          hide={cur <= 0}
-          right='100%'
-          absolute
-          mr />
+      <Block hide={cur >= numSteps}>
+        <Block absolute top='100%' wide h={btnProps.circle} top  bottom m='auto'>
+          <Button
+            icon='keyboard_arrow_left'
+            onClick={local(prev)}
+            {...btnProps}
+            hide={cur <= 0}
+            right='100%'
+            absolute
+            mr />
 
-        <Button
-          icon='keyboard_arrow_right'
-          onClick={local(next)}
-          {...btnProps}
-          left='100%'
-          absolute
-          ml />
+          <Button
+            icon='keyboard_arrow_right'
+            onClick={local(next)}
+            {...btnProps}
+            left='100%'
+            absolute
+            ml />
+        </Block>
+        <Block align='center center' wide absolute top='100%' mt='l'>
+          { dots(cur, local) }
+        </Block>
       </Block>
     </Modal>
+  )
+}
+
+function dots(cur, local) {
+  const arr = Array.apply(null, Array(numSteps))
+  return (
+    arr.map((_, i) => <Block pointer circle='8' bgColor={cur == i ? 'white' : 'rgba(255,255,255,.5)'} mx='s' onClick={local(go, i)}></Block>)
   )
 }
 
@@ -69,6 +81,7 @@ function render ({props, state, local}) {
 
 const next = createAction('<IntroModal/>: next')
 const prev = createAction('<IntroModal/>: prev')
+const go = createAction('<IntroModal/>: go')
 
 /**
  * Reducer
@@ -90,6 +103,18 @@ const reducer = handleActions({
   },
   [next]: state => {
     const cur = Math.min(state.cur + 1, numSteps)
+
+    return {
+      ...state,
+      cur,
+      counts: {
+        ...state.counts,
+        [cur]: (state.counts[cur] || 0) + 1
+      }
+    }
+  },
+  [go]: (state, i) => {
+    const cur = i
 
     return {
       ...state,
