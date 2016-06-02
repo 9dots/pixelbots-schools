65bc6ce589c3c236a500938d506a546e4d5f2405
Andrew Shaffer <andrew@weo.io>
2016-06-02 14:32:35 -0700

    fix description modal (closes #88)

--- src/modals/DescriptionModal/DescriptionModal.js
+++ src/modals/DescriptionModal/DescriptionModal.js
@@ -3,64 +3,81 @@
  */
 
 import {Modal, ModalBody, ModalFooter, ModalHeader, Flex, Block, Text} from 'vdux-ui'
-import {Button, Input, form} from 'vdux-containers'
-import BlockInput from 'components/BlockInput'
+import handleActions from '@f/handle-actions'
+import createAction from '@f/create-action'
+import {Textarea} from 'vdux-containers'
 import {closeModal} from 'reducer/modal'
+import {Button} from 'vdux-containers'
 import element from 'vdux/element'
 import summon from 'vdux-summon'
+import Form from 'vdux-form'
 
 /**
  * <DescriptionModal/>
  */
 
-function render ({props}) {
+function render ({props, local, state}) {
   const {user, changeDescription, fields} = props
   const {aboutMe = ''} = user
-  const len = fields.aboutMe.value
-    ? fields.aboutMe.value.length
-    : aboutMe.length
+  const {text = aboutMe} = state
 
   return (
     <Modal onDismiss={closeModal}>
-      <Flex ui={ModalBody} column align='center center' pb='l'>
-        <ModalHeader>
-          Description
-        </ModalHeader>
-        <BlockInput
-          tag='textarea'
-          defaultValue={aboutMe}
-          name='aboutMe'
-          placeholder='A few words about yourself...'
-          w='300px'
-          p
-          m
-          h='100px'
-          autofocus
-          focusProps={{border: 'rgba(blue, .35)'}}
-          border='rgba(grey, 0.15)'
-          inputProps={{
-            textAlign: 'left',
-            resize: 'none',
-            maxlength: 300,
-            borderStyle: 'none',
-            p: true,
-            outline: 0,
-            h: 111
-          }}/>
-          <Block w='300px'>
-            <Text color='grey_medium' float='right'>{len}/300</Text>
-          </Block>
-      </Flex>
-      <ModalFooter bg='greydark'>
-        <Text fs='xxs'>
-          <Text pointer underline onClick={closeModal}>cancel</Text>
-          <Text mx>or</Text>
-        </Text>
-        <Button type='submit'>Update</Button>
-      </ModalFooter>
+      <Form onSubmit={changeDescription} onSuccess={closeModal}>
+        <Flex ui={ModalBody} column align='center center' pb='l'>
+          <ModalHeader>
+            Description
+          </ModalHeader>
+          <Textarea
+            defaultValue={aboutMe}
+            name='aboutMe'
+            placeholder='A few words about yourself...'
+            onInput={local(setText)}
+            w='300px'
+            p
+            m
+            h='100px'
+            autofocus
+            focusProps={{border: 'rgba(blue, .35)'}}
+            border='rgba(grey, 0.15)'
+            textAlign='left'
+            resize='none'
+            maxlength='300'
+            p
+            outline='0'
+            h='111' />
+            <Block w='300px'>
+              <Text color='grey_medium' float='right'>{text.length}/300</Text>
+            </Block>
+        </Flex>
+        <ModalFooter bg='greydark'>
+          <Text fs='xxs'>
+            <Text pointer underline onClick={closeModal}>cancel</Text>
+            <Text mx>or</Text>
+          </Text>
+          <Button type='submit'>Update</Button>
+        </ModalFooter>
+      </Form>
     </Modal>
   )
 }
+
+/**
+ * Actions
+ */
+
+const setText = createAction('<DescriptionModal/>: set text', e => e.target.value)
+
+/**
+ * Reducer
+ */
+
+const reducer = handleActions({
+  [setText]: (state, text) => ({
+    ...state,
+    text
+  })
+})
 
 /**
  * Exports
@@ -78,14 +95,7 @@ export default summon(({user}) => ({
       }
     }
   })
-}))(
-  form(({changeDescription, user}) => ({
-    fields: ['aboutMe'],
-    onSubmit: function * (model) {
-      yield changeDescription(model)
-      yield closeModal()
-    }
-  }))({
-    render
-  })
-)
+}))({
+  render,
+  reducer
+})
