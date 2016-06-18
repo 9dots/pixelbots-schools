0e66fe5af5fe1751c226304ac2fa50da06b2ce25
Michael Weintraub <mweint@gmail.com>
2016-06-17 18:52:31 -0700

    minor gradebook stuff

--- src/pages/ClassGradebook/Gradebook.js
+++ src/pages/ClassGradebook/Gradebook.js
@@ -2,10 +2,11 @@
  * Imports
  */
 
-import {Table, TableHeader, TableRow, Card, Block} from 'vdux-ui'
+import {Table, TableHeader, TableRow, Card, Block, Icon} from 'vdux-ui'
 import summonChannels from 'lib/summon-channels'
 import GradebookRow from './GradebookRow'
 import Loading from 'components/Loading'
+import {Button, Dropdown, MenuItem} from 'vdux-containers'
 import element from 'vdux/element'
 import map from '@f/map'
 
@@ -14,7 +15,7 @@ import map from '@f/map'
  */
 
 function render ({props}) {
-  const {group, students, activities} = props
+  const {group, students, activities, currentUser} = props
   const {value, loading, loaded} = activities
 
   if (!loaded && loading) return <Loading show={true} h='200' />
@@ -22,17 +23,29 @@ function render ({props}) {
   const {items: activityList} = value
 
   return (
-    <Block w='col_main' mx='auto' my='l' relative overflow='auto' boxShadow='card'>
-      <Table overflow='auto'>
-        <Header activities={activityList} />
-        {
-          map((student, i) => <GradebookRow
-            activities={activityList}
-            student={student}
-            odd={i%2}
-            last={students.length === (i+1)} />,students)
-        }
-      </Table>
+    <Block w='col_main' mx='auto' my='l' relative >
+      <Block align='start center' relative>
+        <Dropdown btn={<DropButton />} z='3' mt={-12} left w='160px' fs='xxs'>
+          <MenuItem py>Display as Percentages </MenuItem>
+          <MenuItem py align='start center'>
+            Export to CSV
+            <Icon name='file_download' fs='xs' ml='s' />
+          </MenuItem>
+        </Dropdown>
+      </Block>
+      <Block boxShadow='card' overflow='auto' relative>
+        <Table overflow='auto'>
+          <Header activities={activityList} />
+          {
+            map((student, i) => <GradebookRow
+              activities={activityList}
+              student={student}
+              odd={i%2}
+              currentUser={currentUser}
+              last={students.length === (i+1)} />,students)
+          }
+        </Table>
+      </Block>
     </Block>
 
   )
@@ -60,8 +73,17 @@ function Header ({props}) {
       {
         map(activity => <TableHeader {...headProps}  fs='xxs' px maxWidth='100px' minWidth='100px' ellipsis>{activity.displayName}</TableHeader>, activities)
       }
-
     </TableRow>
+  )
+}
+
+function DropButton () {
+  return (
+    <Button bgColor='green' mb h={32}>
+      <Icon name='settings' fs='xs' mr='s' />
+      Settings
+      <Icon name='arrow_drop_down' fs='s' ml='s' mr='-12px' />
+    </Button>
   )
 }
 
--- src/pages/ClassGradebook/GradebookRow.js
+++ src/pages/ClassGradebook/GradebookRow.js
@@ -6,14 +6,21 @@ import {TableRow, TableCell, Block} from 'vdux-ui'
 import element from 'vdux/element'
 import map from '@f/map'
 
+/**
+ * Total
+ */
+
+let totalScaled = 0
 
 /**
  * <GradebookRow/>
  */
 
 function render ({props}) {
-  const {student, activities, odd, last} = props
-  const {name: {givenName}, name: {familyName}} = student
+  const {student, activities, odd, last, currentUser} = props
+  const {userType, _id: uid} = currentUser
+  const {name: {givenName}, name: {familyName}, _id: sid} = student
+  const isStudent = userType === 'student'
   const nameCell = {
     borderRightWidth: 0,
     textAlign: 'left',
@@ -23,29 +30,29 @@ function render ({props}) {
     borderBottom: last ? '' : '1px solid #E9E9E9',
     borderRight: '1px solid #E9E9E9',
     textAlign: 'center',
-    p: '16px',
+    p: isStudent ? '40px 16px' : '16px',
   }
+
+  if(isStudent && sid !== uid) return <span/>
 
   return (
     <TableRow borderBottom={last ? '' : '1px solid rgba(black, .1)'}>
       <TableCell {...cellProps} {...nameCell}>
-        {givenName}
+        <Block ellipsis w={80}>
+          {givenName}
+        </Block>
       </TableCell>
       <TableCell {...cellProps} {...nameCell}>
-        {familyName}
+        <Block ellipsis w={80}>
+          {familyName}
+        </Block>
       </TableCell>
       <TableCell reltive {...cellProps} color='blue' bold relative>
-        87%
-        <Block
-          bg='linear-gradient(to right, rgba(52,52,52,0.22), rgba(52,52,52,0))'
-          h='calc(100% + 1px)'
-          left='100%'
-          top='-1px'
-          absolute
-          w='4'/>
+        {Math.round(totalScaled / activities.length * 100) }%
+        <ShadowBlock/>
       </TableCell>
       {
-        map(activity => <GradeCell {...cellProps} activity={activity} student={student}/>, activities)
+        map(activity => <GradeCell activity={activity} student={student} {...cellProps}/>, activities)
       }
     </TableRow>
   )
@@ -53,14 +60,33 @@ function render ({props}) {
 
 function GradeCell ({props}) {
   const {student, activity, ...rest} = props
-
-  const grade = activity.instances.total.length
-    ? activity.instances.total[0].actors[student._id]
+  const {status, instances: {total}} = activity
+  const instance = total.length
+    ? total[0].actors[student._id]
     : false
+
+  if(!instance) return <TableCell {...rest}>-</TableCell>
+
+  totalScaled += instance.status >= 5 ? instance.pointsScaled : 0
+  const scaled = Math.round(instance.pointsScaled * 100) / 100
+  const percent = Math.round(scaled * 100) + '%'
+
   return (
     <TableCell {...rest}>
-      {grade ? grade.pointsScaled : '-' }
+      { instance.status >= 5 ? percent  : '-' }
     </TableCell>
+  )
+}
+
+function ShadowBlock () {
+  return (
+    <Block
+      bg='linear-gradient(to right, rgba(52,52,52,0.22), rgba(52,52,52,0))'
+      h='calc(100% + 1px)'
+      left='100%'
+      top='-1px'
+      absolute
+      w='4'/>
   )
 }
 
