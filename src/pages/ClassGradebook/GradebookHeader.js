0d96567b6efda9a23cd1b107dba5ddb4b4b22782
Michael Weintraub <mweint@gmail.com>
2016-06-23 16:32:00 -0700

    added gradebook sorting

---
+++ src/pages/ClassGradebook/GradebookHeader.js
@@ -0,0 +1,126 @@
+/**
+ * Imports
+ */
+
+import {wrap, CSSContainer, Button, Dropdown} from 'vdux-containers'
+import {TableRow, TableHeader, Icon, Block, Text} from 'vdux-ui'
+import {setUrl} from 'redux-effects-location'
+import element from 'vdux/element'
+import summon from 'vdux-summon'
+import moment from 'moment'
+import map from '@f/map'
+
+/**
+ * Render
+ */
+
+function render ({props}) {
+  const {activities, sort, setSort, ...rest} = props
+  const headProps = {
+    borderRight: '1px solid text',
+    bgColor: 'grey',
+    color: 'white',
+    p: '20px 16px'
+  }
+
+  return (
+    <TableRow>
+      <NameHeader text='First' prop='name.givenName' {...headProps} setPref={setPref} sort={sort}/>
+      <NameHeader text='Last' prop='name.familyName' {...headProps} setPref={setPref} sort={sort} />
+      <TableHeader {...headProps}>
+        Total
+      </TableHeader>
+      {
+        map(activity => <ActivityHeader
+          {...headProps}
+          {...rest}
+          activity={activity} />, activities)
+      }
+    </TableRow>
+  )
+
+  function * setPref(prop) {
+    yield setSort({
+      property: prop,
+      dir: prop === sort.property ? sort.dir * -1 : 1
+    })
+  }
+}
+
+const NameHeader = wrap(CSSContainer, {
+    textAlign: 'left',
+    hoverProps: {
+      hover: true
+    }
+  })({
+  render ({props}) {
+    const {hover, sort, prop, text, setPref, ...rest} = props
+    return (
+      <TableHeader pointer onClick={() => setPref(prop)} {...rest} borderWidth={0}>
+        <Block align='start center'>
+          <Text underline={hover}>
+            {text}
+          </Text>
+          <Icon
+            name={'arrow_drop_' + (sort.dir === 1 ? 'down' : 'up')}
+            hidden={sort.property !== prop}
+            ml='s'
+            fs='s'/>
+        </Block>
+      </TableHeader>
+    )
+  }
+})
+
+const ActivityHeader = wrap(CSSContainer, {
+  hoverProps: {
+    showSettings: true
+  }
+})({
+  render ({props}) {
+    const {activity, showSettings, exportActivity, totalPoints, ...rest} = props
+    const btn = <Icon fs='xxs' pointer absolute='top 0px right 0px' hide={!showSettings} name='info' absolute top={-16} right={-8} />
+
+    return (
+      <TableHeader {...rest}  fs='xxs' px maxWidth='100px' minWidth='100px'>
+        <Dropdown textAlign='left' lighter fs='xs' btn={btn} z='3' minWidth='150px' maxWidth='220px' px mt={-16} right={-8}>
+          <Block bolder>
+            {activity.displayName}
+          </Block>
+          <Block color='blue'>
+            {moment(activity.publishedAt).format('MMM D, YYYY')}
+          </Block>
+          <Block mt='s' mb>
+            Points: {totalPoints(activity)}
+          </Block>
+          <Block align='center center'>
+            <Button pill px py='s' fs='xxs' bgColor='grey_medium' onClick={() =>exportActivity(activity)}>
+              Export to CSV
+            </Button>
+            <Button px py='s' ml='s' fs='xxs' pill onClick={() => setUrl(`/activity/${activity._id}`)}>
+              Go to Activity
+            </Button>
+          </Block>
+        </Dropdown>
+        <Block tall wide ellipsis>
+          {activity.displayName}
+        </Block>
+      </TableHeader>
+    )
+  }
+})
+
+export default summon(() => ({
+  setSort: pref => ({
+    settingSort:  {
+      url: '/preference/gradebookSort',
+      invalidates: '/user',
+      method: 'PUT',
+      body: {
+        value: pref
+      }
+    }
+  })
+}))({
+  render
+})
