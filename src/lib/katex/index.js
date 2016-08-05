f16709606735930ed332f0fe0bed972432fd5b4f
Andrew Shaffer <andrew@weo.io>
2016-08-05 15:51:29 -0700

    fix tooltip dragging issue (closes #206)

--- src/lib/katex/index.js
+++ src/lib/katex/index.js
@@ -1,4 +1,19 @@
+/**
+ * Imports
+ */
+
 const cloudFS = require('cloud-fs')
 
-exports.fontsUrl = cloudFS.url('./static/fonts.css')
-exports.url = cloudFS.url('./static/katex.css')
+/**
+ * Files
+ */
+
+const cssUrl = cloudFS.url('./static/katex.css')
+
+/**
+ * Exports
+ */
+
+export {
+  cssUrl
+}
