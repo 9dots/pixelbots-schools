6d4a3d5794cd29c6663a163704555e54294ef748
Andrew Shaffer <andrew@weo.io>
2016-09-15 15:44:53 -0700

    fix images server

--- src/components/ActivityMedia/ActivityMedia.js
+++ src/components/ActivityMedia/ActivityMedia.js
@@ -130,9 +130,6 @@ const Video = {
             state.play &&
               <Block absolute={{top: 0}} tall wide innerHTML={content} class='activity-video' />
           }
-          {
-            state.play && console.log(object)
-          }
         </Block>
         <Block align='start center' hide printProps={{hide: false}}>
           <Block w={200} relative>
