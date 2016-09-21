7d3bc2552cf110f55b4b5488fc95a7232c431b67
Andrew Shaffer <andrew@weo.io>
2016-09-21 16:40:50 -0700

    use static file servers for assets

--- src/server/page.js
+++ src/server/page.js
@@ -3,14 +3,12 @@
  */
 
 import css from './css'
-import path from 'path'
 import favicon from 'lib/favicon'
 import * as katex from 'lib/katex'
 
 const cloudFS = require('cloud-fs')
 const client = cloudFS.url('./scripts/weo.js')
 const apiStatic = process.env.API_STATIC
-const clientStatic = process.env.STATIC_FILES || ''
 
 /**
  * Page
@@ -28,14 +26,14 @@ function page ({html, state, title}) {
           ${
             css.map(str => `<style>${str}</style>`).join('')
           }
-          <link rel=icon href="${path.join(clientStatic, favicon)}"/>
+          <link rel=icon href="${favicon}"/>
           <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-          <link href="${path.join(clientStatic, katex.cssUrl)}" rel="stylesheet"/>
+          <link href="${katex.cssUrl}" rel="stylesheet"/>
           <script type='text/javascript'>
             window.__initialState__ = ${JSON.stringify(state)}
           </script>
-          <script type='text/javascript' src='${path.join(apiStatic, 'socket.io/socket.io.js')}'></script>
-          <script type='text/javascript' src='${path.join(clientStatic, client)}'></script>
+          <script type='text/javascript' src='${apiStatic + '/socket.io/socket.io.js'}'></script>
+          <script type='text/javascript' src='${client}'></script>
         </head>
         <body>${html}</body>
       </html>
