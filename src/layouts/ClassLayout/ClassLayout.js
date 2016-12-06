ac8e7b2fd2dc38b956a1c513bbacda3bc30f7301
Andrew Shaffer <andrew@weo.io>
2016-12-05 18:14:22 -0800

    dont allow people to view classes theyre not members of (closes #443)

--- src/layouts/ClassLayout/ClassLayout.js
+++ src/layouts/ClassLayout/ClassLayout.js
@@ -4,6 +4,7 @@
 
 import ClassSettingsModal from 'modals/ClassSettingsModal'
 import ClassCodeModal from 'modals/ClassCodeModal'
+import Redirect from 'components/Redirect'
 import {Text, Block, Icon} from 'vdux-ui'
 import FourOhFour from 'pages/FourOhFour'
 import NavTile from 'components/NavTile'
@@ -27,6 +28,7 @@ export default summon(({groupId}) => ({
     const isStudent = currentUser.userType === 'student'
 
     if (!loaded || !students.loaded) return <span />
+    if (currentUser.groups.every(g => g.id !== value._id)) return <Redirect to='/' />
     if (error || students.error) return <FourOhFour />
 
     return (
