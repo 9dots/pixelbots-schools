c1a471f932f77e3c0dca9a8b73f15f4085b91755
Michael Weintraub <michaelweintraub@Michaels-MacBook-Pro.local>
2017-01-10 15:36:41 -0800

    more school settings

--- src/pages/SchoolSettings/SchoolSettings.js
+++ src/pages/SchoolSettings/SchoolSettings.js
@@ -3,8 +3,10 @@
  */
 
 import AvatarPickerModal from 'modals/AvatarPickerModal'
+import ColorPickerModal from 'modals/ColorPickerModal'
 import SettingsRow from 'components/SettingsRow'
 import {component, element} from 'vdux'
+import {pickerColors} from 'lib/colors'
 import Avatar from 'components/Avatar'
 import {Block, Card} from 'vdux-ui'
 
@@ -17,8 +19,13 @@ export default component({
   	const {currentUser} = props
     return (
     	<Card>
+        <SettingsRow name='School' clickable={false} prop={'9 Dots Community Learning Center'} />
+        <SettingsRow name='Location' placeholder='Where is your school located?'/>
         <SettingsRow name='Logo' Modal={<AvatarPickerModal user={currentUser} />}>
           <Avatar sq='40px' actor={currentUser} display='block' />
+        </SettingsRow>
+        <SettingsRow name='Color' Modal={<ColorPickerModal user={currentUser} />} message={`The color that appears in the top bar of your school's page.`}>
+          <Block circle='40px' bgColor={pickerColors[0]} />
         </SettingsRow>
     	</Card>
     )
