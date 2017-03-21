1c199159dcad171f84bb6aeb45c51aa656551eca
Andrew Shaffer <andrew@weo.io>
2017-03-20 17:18:13 -0700

    fix intercom on the client so students dont get included

--- src/lib/segment/segment.js
+++ src/lib/segment/segment.js
@@ -3,50 +3,92 @@
  */
 
 function segment (writeKey) {
-  if (typeof window === 'undefined') {
-    return {}
-  }
+    // Create a queue, but don't obliterate an existing one!
+    var analytics = window.analytics = window.analytics || []
 
-  var analytics = window.analytics = window.analytics || []
-  if (!analytics.initialize) {
+    // If the real analytics.js is already on the page return.
+    if (analytics.initialize) return
+
+    // If the snippet was invoked already show an error.
     if (analytics.invoked) {
-      window.console && console.error && console.error('Segment snippet included twice.')
-    } else {
-      analytics.invoked = !0
-      analytics.methods = ['trackSubmit', 'trackClick', 'trackLink', 'trackForm', 'pageview', 'identify', 'reset', 'group', 'track', 'ready', 'alias', 'page', 'once', 'off', 'on']
-      analytics.factory = function (t) {
-        return function () {
-          var e = Array.prototype.slice.call(arguments)
-          e.unshift(t)
-          analytics.push(e)
-          return analytics
-        }
+      if (window.console && console.error) {
+        console.error('Segment snippet included twice.')
       }
 
-      for (var t = 0; t < analytics.methods.length; t++) {
-        var e = analytics.methods[t]
-        analytics[e] = analytics.factory(e)
-      }
-
-      analytics.load = function (t) {
-        var e = document.createElement('script')
-        e.type = 'text/javascript'
-        e.async = !0
-        e.src = (document.location.protocol === 'https:' ? 'https://' : 'http://')
-          + 'cdn.segment.com/analytics.js/v1/' + t + '/analytics.min.js'
-
-        var n = document.getElementsByTagName('script')[0]
-        n.parentNode.insertBefore(e, n)
-      }
-
-      analytics.SNIPPET_VERSION = '3.1.0'
+      return
     }
-  }
 
-  analytics.load(writeKey)
-  analytics.page()
+    // Invoked flag, to make sure the snippet
+    // is never invoked twice.
+    analytics.invoked = true
 
-  return analytics
+    // A list of the methods in Analytics.js to stub.
+    analytics.methods = [
+      'trackSubmit',
+      'trackClick',
+      'trackLink',
+      'trackForm',
+      'pageview',
+      'identify',
+      'reset',
+      'group',
+      'track',
+      'ready',
+      'alias',
+      'debug',
+      'page',
+      'once',
+      'off',
+      'on'
+    ]
+
+    // Define a factory to create stubs. These are placeholders
+    // for methods in Analytics.js so that you never have to wait
+    // for it to load to actually record data. The `method` is
+    // stored as the first argument, so we can replay the data.
+    analytics.factory = function (method) {
+      return function () {
+        var args = Array.prototype.slice.call(arguments)
+        args.unshift(method)
+        analytics.push(args)
+        return analytics
+      }
+    }
+
+    // For each of our methods, generate a queueing stub.
+    for (var i = 0; i < analytics.methods.length; i++) {
+      var key = analytics.methods[i]
+      analytics[key] = analytics.factory(key)
+    }
+
+    // Define a method to load Analytics.js from our CDN,
+    // and that will be sure to only ever load it once.
+    analytics.load = function (key) {
+      // Create an async script element based on your key.
+      var script = document.createElement('script')
+      script.type = 'text/javascript'
+      script.async = true
+      script.src = ('https:' === document.location.protocol
+        ? 'https://' : 'http://')
+        + 'cdn.segment.com/analytics.js/v1/'
+        + key + '/analytics.min.js'
+
+      // Insert our script next to the first script element.
+      var first = document.getElementsByTagName('script')[0]
+      first.parentNode.insertBefore(script, first)
+    }
+
+    // Add a version to keep track of what's in the wild.
+    analytics.SNIPPET_VERSION = '4.0.0'
+
+    // Load Analytics.js with your key, which will automatically
+    // load the tools you've enabled for your account. Boosh!
+    analytics.load(writeKey)
+
+    // Make the first page call to load the integrations. If
+    // you'd like to manually name or tag the page, edit or
+    // move this call however you'd like.
+    analytics.page()
 }
 
 /**
--- src/middleware/analytics.js
+++ src/middleware/analytics.js
@@ -22,28 +22,54 @@ function analytics () {
   const config = {
     integrations: {
       All: true,
-      Intercom: {
-        hideDefaultLauncher: true
+      Intercom: false
+    }
+  }
+
+  let hasIntercom = false
+  let analyticsReady = false
+  const readyQueue = []
+
+  function getConfig (user) {
+    return {
+      integrations: {
+        All: true,
+        Intercom: hasIntercom
       }
     }
   }
 
-  function hasIntercom (user) {
-    return !!(user._id && user.userType === 'teacher')
-  }
+  window.analytics.ready(() => {
+    analyticsReady = true
+    readyQueue.forEach(fn => fn())
+    readyQueue.length = 0
+  })
 
   return next => action => {
+    if (action.type === identify.type || action.type === track.type || action.type === page.type) {
+      if (analyticsReady) {
+        return process(action)
+      } else {
+        readyQueue.push(() => process(action))
+      }
+    } else {
+      return next(action)
+    }
+  }
+
+  function process (action) {
     switch (action.type) {
       case identify.type: {
         const user = action.payload || {}
 
-        if (typeof Intercom !== 'undefined' && !hasIntercom(user)) {
+        hasIntercom = !!(user._id && user.userType === 'teacher')
+
+        if (typeof Intercom !== 'undefined' && !hasIntercom) {
           window.Intercom('shutdown')
           window.analytics._integrations.Intercom.booted = false
         }
 
-        config.integrations.Intercom.hideDefaultLauncher = !hasIntercom(user)
-        window.analytics.identify(user._id, prep.user(user), config)
+        window.analytics.identify(user._id, prep.user(user), getConfig(user))
         if (window.mixpanel) {
           window.mixpanel.set_config({persistence: 'localStorage'})
         }
@@ -51,16 +77,13 @@ function analytics () {
       }
       case track.type: {
         const {name, traits} = action.payload
-        window.analytics.track(name, traits)
+        window.analytics.track(name, traits, getConfig())
         break
       }
       case page.type: {
         const {name, params} = action.payload
-        window.analytics.page(name, params)
+        window.analytics.page(name, params, getConfig())
         break
-      }
-      default: {
-        return next(action)
       }
     }
   }
