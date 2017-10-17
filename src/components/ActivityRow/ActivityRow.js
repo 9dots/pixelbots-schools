917417f151b2865fd6e4b699996546641528a30f
danleavitt0 <danleavitt0@gmail.com>
2017-10-17 15:16:36 -0700

    switch to correct api server

--- src/components/ActivityRow/ActivityRow.js
+++ src/components/ActivityRow/ActivityRow.js
@@ -4,9 +4,9 @@
 
 import ActivityDropdownMenu from 'components/ActivityDropdownMenu'
 import ActivityCardActions from 'components/ActivityCardActions'
-import {stopPropagation, component, element} from 'vdux'
-import {wrap, CSSContainer, Button} from 'vdux-containers'
-import {Flex, Block, Card} from 'vdux-ui'
+import { stopPropagation, component, element } from 'vdux'
+import { wrap, CSSContainer, Button } from 'vdux-containers'
+import { Flex, Block, Card } from 'vdux-ui'
 import WeoIcon from 'components/WeoIcon'
 import BgImg from 'components/BgImg'
 import Meta from './Meta'
@@ -16,115 +16,162 @@ import Meta from './Meta'
  */
 
 export default wrap(CSSContainer, {
-  hoverProps: {hover: true}
-})(component({
-  render ({props, context, state, actions}) {
-    const {
-      hover, activity, metaUi: MetaUi = Meta, ddMenu,
-      badgeUi: BadgeUi = Badge, currentUser = {}, options, showClass
-    } = props
-    const {image, displayName, description, _id: id, pinned} = activity
-    const {isUpdatingPin} = state
+  hoverProps: { hover: true }
+})(
+  component({
+    render({ props, context, state, actions }) {
+      const {
+        hover,
+        activity,
+        metaUi: MetaUi = Meta,
+        ddMenu,
+        badgeUi: BadgeUi = Badge,
+        currentUser = {},
+        options,
+        showClass
+      } = props
+      const { image, displayName, description, _id: id, pinned } = activity
+      const { isUpdatingPin } = state
 
-    const btnProps = pinned 
-      ? {bgColor: 'red', color: 'white'}
-      : {bgColor: 'white', color: 'red'}
+      const btnProps = pinned
+        ? { bgColor: 'red', color: 'white' }
+        : { bgColor: 'white', color: 'red' }
 
-    return (
-      <Card h={132} wide mt={0} borderBottom='rgba(52, 52, 52, 0.08)' cursor='pointer' onClick={context.setUrl(`/activity/${activity.groupId}/${activity.playlistRef}`)} >
-        <Flex tall align='start start'>
-          <Flex p='m' tall column align='space-between' flex='49%'>
-            <Block fs='s' fw='200' ellipsis>{displayName}</Block>
-            <Block fs='xs' lh='20px' maxHeight='40px' overflow='hidden' fw='200' color='text'>{description}</Block>
-            <MetaUi activity={activity} currentUser={currentUser} showClass={showClass} />
-          </Flex>
-          <Block flex='22%' mr>
-            {
-            image && image.url &&
-              <BgImg
-                backgroundPosition='center center'
-                img={image.url}
-                thumb={350}
-                backgroundSize='cover'
-                overflow='hidden'
-                boxShadow='card'
-                wide
-                h='108px'
-                relative
-                rounded
-                my='m' />
-          }
-          </Block>
-          <Flex column flex='28%' pl align='space-between' tall>
-            {
-              props.badgeUi &&
-                <Flex flex align='end start'>
+      return (
+        <Card
+          h={132}
+          wide
+          mt={0}
+          borderBottom="rgba(52, 52, 52, 0.08)"
+          cursor="pointer"
+          onClick={context.setUrl(
+            `/activity/${activity.groupId}/${activity.playlistRef}`
+          )}
+        >
+          <Flex tall align="start start">
+            <Flex p="m" tall column align="space-between" flex="49%">
+              <Block fs="s" fw="200" ellipsis>
+                {displayName}
+              </Block>
+              <Block
+                fs="xs"
+                lh="20px"
+                maxHeight="40px"
+                overflow="hidden"
+                fw="200"
+                color="text"
+              >
+                {description}
+              </Block>
+              <MetaUi
+                activity={activity}
+                currentUser={currentUser}
+                showClass={showClass}
+              />
+            </Flex>
+            <Block flex="22%" mr>
+              {image &&
+                image.url && (
+                  <BgImg
+                    backgroundPosition="center center"
+                    img={image.url}
+                    thumb={350}
+                    backgroundSize="cover"
+                    overflow="hidden"
+                    boxShadow="card"
+                    wide
+                    h="108px"
+                    relative
+                    rounded
+                    my="m"
+                  />
+                )}
+            </Block>
+            <Flex column flex="28%" pl align="space-between" tall>
+              {props.badgeUi && (
+                <Flex flex align="end start">
                   <BadgeUi activity={activity} currentUser={currentUser} />
                 </Flex>
-            }
-            {
-              // (hover || pinned) &&
-              //   <Flex align='end center'>
-              //     <Block onClick={stopPropagation}>
-              //       <Button disabled={isUpdatingPin} m py='s' px hoverProps={{highlight: .01}} focusProps={{highlight: .02}} {...btnProps} onClick={actions.pin}>
-              //         <WeoIcon name='pin' fs='21px' mb={-2} />
-              //       </Button>
-              //     </Block>
-              //   </Flex>
-            }
-            {
-              options &&
-                <Flex column wide align='space-between end' tall  mb='s'>
+              )}
+              {
+                // (hover || pinned) &&
+                //   <Flex align='end center'>
+                //     <Block onClick={stopPropagation}>
+                //       <Button disabled={isUpdatingPin} m py='s' px hoverProps={{highlight: .01}} focusProps={{highlight: .02}} {...btnProps} onClick={actions.pin}>
+                //         <WeoIcon name='pin' fs='21px' mb={-2} />
+                //       </Button>
+                //     </Block>
+                //   </Flex>
+              }
+              {options && (
+                <Flex column wide align="space-between end" tall mb="s">
                   {
                     <Block hidden={!pinned} sq={60} relative>
-                      <Block 
+                      <Block
                         sq={60}
-                        border='30px solid transparent'
-                        borderTopColor='red'
-                        borderRightColor='red'
-                        />
-                      <WeoIcon name='pin' color='white' absolute top right mt={8} mr={5} />
+                        border="30px solid transparent"
+                        borderTopColor="red"
+                        borderRightColor="red"
+                      />
+                      <WeoIcon
+                        name="pin"
+                        color="white"
+                        absolute
+                        top
+                        right
+                        mt={8}
+                        mr={5}
+                      />
                     </Block>
                   }
-                  {
-                    ddMenu &&
-                      <Block mr ml='-6' onClick={stopPropagation} disabled={isUpdatingPin}>
-                        <ActivityDropdownMenu pinAction={actions.pin} key={activity.kye} activity={activity} />
-                      </Block>
-                  }
+                  {ddMenu && (
+                    <Block
+                      mr
+                      ml="-6"
+                      onClick={stopPropagation}
+                      disabled={isUpdatingPin}
+                    >
+                      <ActivityDropdownMenu
+                        pinAction={actions.pin}
+                        key={activity.kye}
+                        activity={activity}
+                      />
+                    </Block>
+                  )}
                 </Flex>
-            }
+              )}
+            </Flex>
           </Flex>
-        </Flex>
-      </Card>
-    )
-  },
-  controller: {
-    * pin ({props, actions, context}) {
-      const {activity} = props
-      const {groupId, playlistRef, pinned, key} = activity
+        </Card>
+      )
+    },
+    controller: {
+      *pin({ props, actions, context }) {
+        const { activity } = props
+        const { groupId, playlistRef, pinned, key } = activity
 
-      yield actions.updatingPin(true)
-      yield [
-        context.fetch(process.env.CLOUD_FUNCTION_SERVER + '/pinAssignment', {
-          method: 'POST',
-          headers: {'CONTENT-TYPE': 'application/json'},
-          body: JSON.stringify({groupId, playlistRef, pinned})
-        }),
-        context.firebaseUpdate(`/feed/${groupId}/${key}`, {pinned: !pinned})
-      ]
-      yield actions.updatingPin(false)
+        yield actions.updatingPin(true)
+        yield [
+          context.fetch(process.env.API_SERVER + '/pinAssignment', {
+            method: 'POST',
+            headers: { 'CONTENT-TYPE': 'application/json' },
+            body: JSON.stringify({ groupId, playlistRef, pinned })
+          }),
+          context.firebaseUpdate(`/feed/${groupId}/${key}`, { pinned: !pinned })
+        ]
+        yield actions.updatingPin(false)
+      }
+    },
+    reducer: {
+      updatingPin: (state, isUpdatingPin) => ({ isUpdatingPin })
     }
-  },
-  reducer: {
-    updatingPin: (state, isUpdatingPin) => ({isUpdatingPin})
-  }
-}))
+  })
+)
 
 /**
  * <Badge/>
  */
 
-function Badge () {
+function Badge() {
   return <span />
 }
--- src/modals/CreateStudentModal/CreateStudentModal.js
+++ src/modals/CreateStudentModal/CreateStudentModal.js
@@ -2,10 +2,17 @@
  * Imports
  */
 
-import {Modal, ModalBody, ModalFooter, ModalHeader, Block, Text} from 'vdux-ui'
+import {
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+  Block,
+  Text
+} from 'vdux-ui'
 import LineInput from 'components/LineInput'
-import {component, element} from 'vdux'
-import {Button} from 'vdux-containers'
+import { component, element } from 'vdux'
+import { Button } from 'vdux-containers'
 import validate from 'lib/validate'
 import summon from 'vdux-summon'
 import Form from 'vdux-form'
@@ -15,34 +22,45 @@ import Form from 'vdux-form'
  */
 
 export default component({
-  render ({props, actions, context, state}) {
-    const {creatingStudent = {}, joiningClass = {}} = props
-    const {fetching} = state
+  render({ props, actions, context, state }) {
+    const { creatingStudent = {}, joiningClass = {} } = props
+    const { fetching } = state
     const loading = creatingStudent.loading || joiningClass.loading
 
     return (
       <Modal onDismiss={context.closeModal}>
-        <Form onSubmit={actions.checkStudentCredentials} validate={validateStudent} autocomplete='off'>
-          <ModalBody pb='l' w='col_m' mx='auto'>
-            <ModalHeader>
-              Create New Student
-            </ModalHeader>
-            <input type='hidden' name='userType' value='student' />
-            <Block align='start center'>
-              <LineInput autofocus name='name[givenName]' mr='l' placeholder='First Name' />
-              <LineInput name='name[familyName]' placeholder='Last Name' />
+        <Form
+          onSubmit={actions.checkStudentCredentials}
+          validate={validateStudent}
+          autocomplete="off"
+        >
+          <ModalBody pb="l" w="col_m" mx="auto">
+            <ModalHeader>Create New Student</ModalHeader>
+            <input type="hidden" name="userType" value="student" />
+            <Block align="start center">
+              <LineInput
+                autofocus
+                name="name[givenName]"
+                mr="l"
+                placeholder="First Name"
+              />
+              <LineInput name="name[familyName]" placeholder="Last Name" />
             </Block>
-            <Block align='start center' my='l'>
-              <LineInput name='username' mr='l' placeholder='Username' />
-              <LineInput name='email' placeholder='email (Optional)' />
+            <Block align="start center" my="l">
+              <LineInput name="username" mr="l" placeholder="Username" />
+              <LineInput name="email" placeholder="email (Optional)" />
             </Block>
           </ModalBody>
-          <ModalFooter bg='grey'>
-            <Text fs='xxs'>
-              <Text pointer underline onClick={!fetching && context.closeModal}>cancel</Text>
+          <ModalFooter bg="grey">
+            <Text fs="xxs">
+              <Text pointer underline onClick={!fetching && context.closeModal}>
+                cancel
+              </Text>
               <Text mx>or</Text>
             </Text>
-            <Button disabled={fetching} type='submit' busy={loading}>Create</Button>
+            <Button disabled={fetching} type="submit" busy={loading}>
+              Create
+            </Button>
           </ModalFooter>
         </Form>
       </Modal>
@@ -50,15 +68,22 @@ export default component({
   },
 
   controller: {
-    * createStudentAndJoin ({props, context, actions}, model) {
-      const {value} = yield context.fetch(`${process.env.CLOUD_FUNCTION_SERVER}/createNewUser`, {
-        method: 'POST',
-        headers: {'CONTENT-TYPE': 'application/json'},
-        body: JSON.stringify({...model, classId: props.groupId, email: model.email || undefined})
-      })
+    *createStudentAndJoin({ props, context, actions }, model) {
+      const { value } = yield context.fetch(
+        `${process.env.API_SERVER}/createNewUser`,
+        {
+          method: 'POST',
+          headers: { 'CONTENT-TYPE': 'application/json' },
+          body: JSON.stringify({
+            ...model,
+            classId: props.groupId,
+            email: model.email || undefined
+          })
+        }
+      )
       yield context.closeModal()
     },
-    * addStudentToClass ({context, props}, user) {
+    *addStudentToClass({ context, props }, user) {
       yield [
         context.firebaseUpdate(`/classes/${props.groupId}/students`, {
           [user.uid]: true
@@ -69,66 +94,82 @@ export default component({
       ]
       yield context.closeModal()
     },
-    * checkStudentCredentials ({props, context, actions}, model) {
+    *checkStudentCredentials({ props, context, actions }, model) {
       yield actions.isFetching(true)
-      const {value} = yield context.fetch(`${process.env.CLOUD_FUNCTION_SERVER}/checkUserEmail`, {
-        method: 'POST',
-        headers: {'CONTENT-TYPE': 'application/json'},
-        body: JSON.stringify({...model, classId: props.groupId, email: model.email || undefined})
-      })
+      const { value } = yield context.fetch(
+        `${process.env.API_SERVER}/checkUserEmail`,
+        {
+          method: 'POST',
+          headers: { 'CONTENT-TYPE': 'application/json' },
+          body: JSON.stringify({
+            ...model,
+            classId: props.groupId,
+            email: model.email || undefined
+          })
+        }
+      )
       if (value.status === 'success') {
         yield actions.createStudentAndJoin(model)
       } else {
-        const {field, message, user} = value.payload
+        const { field, message, user } = value.payload
         if (field === 'email') {
-          yield context.openModal(() => <OverwriteStudent {...actions} groupId={props.groupId} user={user} />)
+          yield context.openModal(() => (
+            <OverwriteStudent
+              {...actions}
+              groupId={props.groupId}
+              user={user}
+            />
+          ))
         } else {
           yield actions.isFetching(false)
-          throw ([{field, message}])
+          throw [{ field, message }]
         }
       }
     }
   },
 
   reducer: {
-    isFetching: (state, fetching) => ({fetching})
+    isFetching: (state, fetching) => ({ fetching })
   }
 })
 
 const OverwriteStudent = component({
-  initialState ({props}) {
+  initialState({ props }) {
     return {
       user: props.user
     }
   },
-  render ({props, state, context}) {
-    const {fetching, user} = state
+  render({ props, state, context }) {
+    const { fetching, user } = state
     const [firstName, lastName] = user.username
     return (
       <Modal onDismiss={context.closeModal}>
-        <Form  autocomplete='off'>
-          <ModalBody pb='l' w='col_m' mx='auto'>
-            <ModalHeader>
-              Create New Student
-            </ModalHeader>
+        <Form autocomplete="off">
+          <ModalBody pb="l" w="col_m" mx="auto">
+            <ModalHeader>Create New Student</ModalHeader>
             <Block>
-              That email is in use for the username <b>{user.username}</b>. Would you like to add this person to your class?
+              That email is in use for the username <b>{user.username}</b>.
+              Would you like to add this person to your class?
             </Block>
           </ModalBody>
-          <ModalFooter bg='grey'>
-            <Text fs='xxs'>
-              <Text pointer underline onClick={!fetching && context.closeModal}>cancel</Text>
+          <ModalFooter bg="grey">
+            <Text fs="xxs">
+              <Text pointer underline onClick={!fetching && context.closeModal}>
+                cancel
+              </Text>
               <Text mx>or</Text>
             </Text>
-            <Button onClick={props.addStudentToClass(user)}>Add To Class</Button>
+            <Button onClick={props.addStudentToClass(user)}>
+              Add To Class
+            </Button>
           </ModalFooter>
         </Form>
       </Modal>
     )
   },
   reducer: {
-    isFetching: (state, fetching) => ({fetching}),
-    updateUser: (state, obj) => ({...state.user, ...obj})
+    isFetching: (state, fetching) => ({ fetching }),
+    updateUser: (state, obj) => ({ ...state.user, ...obj })
   }
 })
 
@@ -136,8 +177,11 @@ const OverwriteStudent = component({
  * validateStudent
  */
 
-function validateStudent (model) {
-  const result = validate.student({...model, email: model.email ? model.email : undefined})
+function validateStudent(model) {
+  const result = validate.student({
+    ...model,
+    email: model.email ? model.email : undefined
+  })
   model.tmpPassword = model.password
 
   return result
